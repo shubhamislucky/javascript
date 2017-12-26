2546041b641a9562e04b03b54097e21a901cb316
Shubham Singh <shubhamsinghtitanfall@gmail.com>
2017-12-26 17:08:02 +0530

    new stuff

---
+++ js/basic.js
@@ -0,0 +1,26 @@
+var hello;
+hello = new Date();
+document.write(hello);
+
+var wish = "Merry Christmas everyone!"
+document.write("<br>" + wish);
+
+// single line comment in javascript
+
+/* multiple
+   line comment in
+   javascript
+*/
+
+name = 'Harsh'
+age = 17;
+ismarried = false;
+profession = 'student';
+
+document.write('<br>' + name + ' is a ' + age + ' year old ' + profession
+                + '.' + ' Is he married? ' + ismarried + '.');
+
+/*
+var day = prompt("which day is today?");
+document.write('<br>' + 'Today is ' + day + '.');
+*/
---
+++ js/operator.js
@@ -0,0 +1,6 @@
+var age = 34;
+var name = 'Lucky';
+document.write('<br>');
+document.write(name + age);
+document.write('<br>');
+document.write(age + age);
